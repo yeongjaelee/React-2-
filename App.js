730c526d14c4301e26d7bac9c385c392d096caee
yeongjae_lee <31798849+yeongjaelee@users.noreply.github.com>
2022-04-05 09:24:12 +0000

    Add files via upload

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import InputSample from './InputSample';
+import UserList from './UserList';
+
 
 function App() {
   return (
-    <InputSample />
+    <UserList />
   );
 }
-
 
 export default App;
